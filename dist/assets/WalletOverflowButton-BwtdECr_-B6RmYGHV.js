2654a49b2fb1309a61f497a4822c5f09dcbedb0d
Cyano88 <onojacyano@gmail.com>
2026-06-02 11:39:48 +0100

    Serve prebuilt frontend on Render

---
+++ dist/assets/WalletOverflowButton-BwtdECr_-B6RmYGHV.js
@@ -0,0 +1,1 @@
+import{di as s,e8 as o,e7 as r,e9 as t}from"./index-BfWSIK0y.js";import{W as l}from"./wallet-O4wj5Eln.js";const a=({onClick:e,text:i})=>s.jsxs(o,{onClick:e,children:[s.jsx(r,{children:s.jsx(l,{})}),s.jsx(t,{children:i})]});export{a as l};
