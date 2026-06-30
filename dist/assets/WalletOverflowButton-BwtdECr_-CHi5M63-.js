f8ac72355f2f80aef441648a44efc1485a0e04a4
Cyano88 <onojacyano@gmail.com>
2026-06-30 16:01:32 +0100

    Use prebuilt frontend for Render deploy

---
+++ dist/assets/WalletOverflowButton-BwtdECr_-CHi5M63-.js
@@ -0,0 +1,1 @@
+import{j as s,bS as t,bx as x,bT as i}from"./index-CYlmoJDc.js";import{W as l}from"./wallet-DYnXMQt_.js";const a=({onClick:o,text:r})=>s.jsxs(t,{onClick:o,children:[s.jsx(x,{children:s.jsx(l,{})}),s.jsx(i,{children:r})]});export{a as l};
