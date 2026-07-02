e4e7d663a05b97732aad879c6a68810f6e759b0b
Cyano88 <onojacyano@gmail.com>
2026-07-02 21:37:27 +0100

    Rebuild frontend bundle for Paycrest POS option

--- dist/assets/WalletOverflowButton-BwtdECr_-u_86Hasn.js
+++
@@ -1,1 +0,0 @@
-import{j as s,bS as t,bx as x,bT as i}from"./index-Dd5N-53P.js";import{W as l}from"./wallet-Bfvxc1oz.js";const a=({onClick:o,text:r})=>s.jsxs(t,{onClick:o,children:[s.jsx(x,{children:s.jsx(l,{})}),s.jsx(i,{children:r})]});export{a as l};
