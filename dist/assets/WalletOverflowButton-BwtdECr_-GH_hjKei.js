b34979d89b253ee214e0510db802e097e66ee99b
Cyano88 <onojacyano@gmail.com>
2026-07-08 01:14:27 +0100

    Use valid Polymarket proxy signature type

---
+++ dist/assets/WalletOverflowButton-BwtdECr_-GH_hjKei.js
@@ -0,0 +1,1 @@
+import{j as s,bS as t,bx as x,bT as i}from"./index-Bv-E6cIb.js";import{W as l}from"./wallet-CSU4wDsm.js";const a=({onClick:o,text:r})=>s.jsxs(t,{onClick:o,children:[s.jsx(x,{children:s.jsx(l,{})}),s.jsx(i,{children:r})]});export{a as l};
