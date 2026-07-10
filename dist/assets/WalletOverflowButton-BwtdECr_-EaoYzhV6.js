5c6d56d37585b3a307ee97a4d28cb8744c105342
Cyano88 <onojacyano@gmail.com>
2026-07-10 11:43:24 +0100

    Revert "Replace smart funding checkout CTA"
    This reverts commit a9ca67276c06ef0c214723109ea3517389834c7c.

---
+++ dist/assets/WalletOverflowButton-BwtdECr_-EaoYzhV6.js
@@ -0,0 +1,1 @@
+import{j as s,bS as t,bx as x,bT as i}from"./index-CyxCILeq.js";import{W as l}from"./wallet-ik7QpRMz.js";const a=({onClick:o,text:r})=>s.jsxs(t,{onClick:o,children:[s.jsx(x,{children:s.jsx(l,{})}),s.jsx(i,{children:r})]});export{a as l};
--- dist/assets/axios-BnQsMgsp.js
+++
@@ -1,1 +0,0 @@
-import{bl as o}from"./index-DEkKlEVX.js";import{r}from"./axios-nZQ4TIzi.js";var s=r();const i=o(s);export{i as a};
