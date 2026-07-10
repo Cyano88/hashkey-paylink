a989022c87aa93adfe3f5181ea30a80407fef862
Cyano88 <onojacyano@gmail.com>
2026-07-10 21:43:19 +0100

    Fix live position tab classification

--- dist/assets/axios-DnfA2b0m.js
+++
@@ -1,1 +0,0 @@
-import{bl as o}from"./index-i2uK6pCI.js";import{r}from"./axios-TjCza-OU.js";var s=r();const i=o(s);export{i as a};
--- dist/assets/shouldProceedtoEmbeddedWalletCreationFlow-D74vhcr8-Bxs88r5y.js
+++
@@ -1,1 +0,0 @@
-import{bd as o,be as n}from"./index-i2uK6pCI.js";const t=(e,a)=>o(e,a.ethereum.createOnLogin)||n(e,a.solana.createOnLogin);export{t as a};
